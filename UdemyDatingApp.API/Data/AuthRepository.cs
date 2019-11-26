1d07b53d7eb118b271710b5e102079b3d35cf558
Nathan Pabst <Nathan.Pabst@ey.com>
2019-11-26 10:44:23 -0600

    adding the Login method logic

--- UdemyDatingApp.API/Data/AuthRepository.cs
+++ UdemyDatingApp.API/Data/AuthRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using UdemyDatingApp.API.Models;
 
 namespace UdemyDatingApp.API.Data
@@ -12,9 +13,33 @@ namespace UdemyDatingApp.API.Data
             _context = context;
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) 
+            {
+                return null;
+            }
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            }        
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
