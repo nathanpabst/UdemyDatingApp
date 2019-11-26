d34b2a97609483fc1a2f4312a2783024c2a3d87b
Nathan Pabst <Nathan.Pabst@ey.com>
2019-11-26 10:15:36 -0600

    adding initial auth setup and the Register method

--- UdemyDatingApp.API/Data/DataContext.cs
+++ UdemyDatingApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace UdemyDatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ UdemyDatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using UdemyDatingApp.API.Models;
+
+namespace UdemyDatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+
+         Task<User> Login(string username, string password);
+
+         Task<bool> UserExists(string username);
+
+    }
+}
