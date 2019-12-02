c51fdddec3432bb64cdb436c543573f2e335b939
Nathan Pabst <Nathan.Pabst@ey.com>
2019-12-02 09:24:46 -0600

    adding dtos folder and data object for registration.

--- UdemyDatingApp.API/Controllers/AuthController.cs
+++ UdemyDatingApp.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using UdemyDatingApp.API.Data;
+using UdemyDatingApp.API.Dtos;
 using UdemyDatingApp.API.Models;
 
 namespace UdemyDatingApp.API.Controllers
@@ -16,23 +17,23 @@ namespace UdemyDatingApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // TODO: validate request
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await _repo.UserExists(username))
+            if (await _repo.UserExists(userForRegisterDto.Username))
             {
                 return BadRequest("Username already exists");
             }
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
