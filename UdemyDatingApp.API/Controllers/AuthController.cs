5b29526975a96a54565d62a7579c0b380f963f2a
Nathan Pabst <Nathan.Pabst@ey.com>
2019-12-07 12:56:02 -0600

    code to implement a token for auth.

--- UdemyDatingApp.API/Controllers/AuthController.cs
+++ UdemyDatingApp.API/Controllers/AuthController.cs
@@ -1,25 +1,33 @@
+using System.Security.Claims;
+using System;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using UdemyDatingApp.API.Data;
 using UdemyDatingApp.API.Dtos;
 using UdemyDatingApp.API.Models;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace UdemyDatingApp.API.Controllers
 {
     [Route("api/[controller]")]
-   [ApiController]
+    [ApiController]
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            
+
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
@@ -35,6 +43,51 @@ namespace UdemyDatingApp.API.Controllers
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            // check db for existing user and matching credentials
+            var userFromRepo = await _repo.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            // building the token with ID and username claims
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            // ensuring the token that comes back is valid
+            // creates the key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            // adding key to the signing credentials and encrypting the key with a hashing algorithm
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // adding description to the token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            // create the token handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // create the token with description
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // write token into the response back to the client
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+
         }
     }
 }
