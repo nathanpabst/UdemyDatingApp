6f7f00f2d905613a3391c82d66b9f63924920a8f
Nathan Pabst <Nathan.Pabst@ey.com>
2019-12-18 16:44:55 -0600

    wip

--- UdemyDatingApp-SPA/src/app/nav/nav.component.ts
+++ UdemyDatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {
 
@@ -18,9 +19,9 @@ export class NavComponent implements OnInit {
 
   login() {
       this.authService.login(this.model).subscribe(next => {
-        console.log('logged in successfully');
+        this.alertify.success('logged in successfully');
       }, error => {
-        console.log(error);
+        this.alertify.error(error);
       });
   }
 
@@ -31,6 +32,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 }
