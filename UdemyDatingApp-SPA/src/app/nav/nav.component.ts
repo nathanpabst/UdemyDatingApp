3d0ad4159fa10908dcba393cdd9900c72ff2cdcc
Nathan Pabst <Nathan.Pabst@ey.com>
2020-01-04 13:01:47 -0600

    adding route guards.

--- UdemyDatingApp-SPA/src/app/nav/nav.component.ts
+++ UdemyDatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
@@ -11,18 +12,25 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) {}
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   login() {
-      this.authService.login(this.model).subscribe(next => {
+    this.authService.login(this.model).subscribe(
+      next => {
         this.alertify.success('logged in successfully');
-      }, error => {
+      },
+      error => {
         this.alertify.error(error);
-      });
+      }, () => {
+        this.router.navigate(['/members']);
+      }
+    );
   }
 
   loggedIn() {
@@ -32,5 +40,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
